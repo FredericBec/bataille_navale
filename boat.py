4f734ca59025b8a7621d2680ac44f2ed75b7d3f5
Frederic Bec <bec.frederic90@outlook.fr>
2025-01-21 13:19:12 +0100

    modify boat

--- boat.py
+++ boat.py
@@ -4,15 +4,7 @@ from typing import Tuple, ClassVar
 
 @dataclass
 class Boat:
-    boats: ClassVar[list['boats']] = []
     position: list[Tuple[int, int]]
     direction: int
     tall: int
     sink: bool
-
-    @classmethod
-    def nb_boats(cls) -> int:
-        return len(cls.boats)
-
-    def __post_init__(self):
-        Boat.boats.append(self)
